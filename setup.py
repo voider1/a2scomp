70ebd321503c4ba13c7c120129a6a5d5efbf4696
voider1 <wesleyey0408@gmail.com>
2017-02-22 16:34:47 +0100

    Making it easier to install thet ool

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='a2scomp',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[
+        'a2scomp/config.py',
+        'a2scomp/apktool.py',
+        'a2scomp/zipalign.py',
+        'a2scomp/apksign.py',
+    ],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        a2scomp=a2scomp.a2scomp:cli
+    ''',
+)
