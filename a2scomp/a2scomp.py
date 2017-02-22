a25edf1c05dc2b5ae5e587e61f1c221b870a92e5
voider1 <wesleyey0408@gmail.com>
2017-02-22 16:35:32 +0100

    Making this the entry point for the tool

--- a2scomp/a2scomp.py
+++ a2scomp/a2scomp.py
@@ -1,31 +1,21 @@
-import argparse
-import sys
+#!../venv/bin/python
+import click
 
-from apktool import decode_apk, build_apk
-
-
-def test_apk(apk_path):
-    decoded_apk = apk_path.split('/')[-1][:-4]
-
-    print('Going to decode the APK...')
-    decode_apk(apk_path)
-    print('[+] Succesfully decoded the APK')
-    print('Going to build the APK...')
-    build_apk(decoded_apk)
-    print('[+] Succesfully built the APK')
+from config import Config
 
 
-if __name__ == '__main__':
-    apk_extension = '.apk'
-    parser = argparse.ArgumentParser()
-    parser.add_argument('apk')
-    args = parser.parse_args()
-    apk_path = args.apk
+@click.group()
+@click.option('--apk', type=click.Path(exists=True),
+              help='APK to perform operation on')
+@click.option('--smali', type=click.Path(exists=True),
+              help='Smali to perform operation on')
+@click.option('--keystore', type=click.Path(exists=True),
+              help='Keystore to use for signing')
+@click.pass_context
+def cli(ctx, apk, smali, keystore):
+    ctx.obj = Config(apk, smali, keystore)
 
-    if apk_path[-4:] != apk_extension:
-        print('[-] The APK doesn\'t end in .apk, make sure it ends in .apk')
-        sys.exit()
 
-    print('Going to test the APK...')
-    test_apk(apk_path)
-    print('[+] The APK is succesfully tested')
+import apktool # NOQA
+import zipalign # NOQA
+import apksign # NOQA
